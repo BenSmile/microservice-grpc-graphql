3b2a5e1a7058ed8586593f130ae70be82a928b77
Smile-Benjamin <Smile.Benjamin@vodacom.cd>
2024-10-19 19:24:04 +0100

    configure graphql server | mutations & queries

---
+++ graphql/graph.go
@@ -0,0 +1,61 @@
+package main
+
+import (
+	"github.com/99designs/gqlgen/graphql"
+	"github.com/bensmile/microservice-grpc-graphql/account"
+)
+
+type Server struct {
+	accountClient *account.Client
+	// catalogClient *catalog.Client
+	// orderClient   *order.Client
+}
+
+func NewGraphqlServer(accountUrl, catalogUrl, orderUrl string) (*Server, error) {
+
+	accountClient, err := account.NewClient(accountUrl)
+	if err != nil {
+		accountClient.Close()
+		return nil, err
+	}
+	// catalogClient, err := catalog.NewClient(catalogUrl)
+	// if err != nil {
+	// 	catalogClient.Close()
+	// 	return nil, err
+	// }
+	// orderClient, err := order.NewClient(orderUrl)
+	// if err != nil {
+	// 	orderClient.Close()
+	// 	return nil, err
+	// }
+
+	return &Server{
+		accountClient,
+		// catalogClient,
+		// orderClient,
+	}, nil
+}
+
+func (s *Server) Mutation() MutationResolver {
+	return &mutationResolver{
+		server: s,
+	}
+}
+
+func (s *Server) Query() QueryResolver {
+	return &queryResolver{
+		server: s,
+	}
+}
+
+func (s *Server) Account() AccountResolver {
+	return &accountResolver{
+		server: s,
+	}
+}
+
+func (s *Server) ToExecutableSchema() graphql.ExecutableSchema {
+	return NewExecutableSchema(Config{
+		Resolvers: s,
+	})
+}
--- graphql/mutation_resolver.go
+++ graphql/mutation_resolver.go
@@ -1,5 +1,17 @@
-// CreateAccounts
+package main
 
-// CreateProducts
+import "context"
 
-// CreateOrder
+type mutationResolver struct {
+	server *Server
+}
+
+func (s *mutationResolver) CreateAccount(ctx context.Context, account *AccountInput) (*Account, error) {
+	return nil, nil
+}
+func (s *mutationResolver) CreateProduct(ctx context.Context, product *ProductInput) (*Product, error) {
+	return nil, nil
+}
+func (s *mutationResolver) CreateOrder(ctx context.Context, order *OrderInput) (*Order, error) {
+	return nil, nil
+}
