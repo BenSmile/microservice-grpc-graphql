ba9cf4a0f84aae7cd44a8ca7a10f7773798b5453
Smile-Benjamin <Smile.Benjamin@vodacom.cd>
2024-10-23 07:10:38 +0100

    order client added to graphql server

--- graphql/graph.go
+++ graphql/graph.go
@@ -4,12 +4,13 @@ import (
 	"github.com/99designs/gqlgen/graphql"
 	"github.com/bensmile/microservice-grpc-graphql/account"
 	"github.com/bensmile/microservice-grpc-graphql/catalog"
+	"github.com/bensmile/microservice-grpc-graphql/order"
 )
 
 type Server struct {
 	accountClient *account.Client
 	catalogClient *catalog.Client
-	// orderClient   *order.Client
+	orderClient   *order.Client
 }
 
 func NewGraphqlServer(accountUrl, catalogUrl, orderUrl string) (*Server, error) {
@@ -24,16 +25,16 @@ func NewGraphqlServer(accountUrl, catalogUrl, orderUrl string) (*Server, error) 
 		catalogClient.Close()
 		return nil, err
 	}
-	// orderClient, err := order.NewClient(orderUrl)
-	// if err != nil {
-	// 	orderClient.Close()
-	// 	return nil, err
-	// }
+	orderClient, err := order.NewClient(orderUrl)
+	if err != nil {
+		orderClient.Close()
+		return nil, err
+	}
 
 	return &Server{
 		accountClient,
 		catalogClient,
-		// orderClient,
+		orderClient,
 	}, nil
 }
 
